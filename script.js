8a2a0d8e0a12d4d8f91b6bcacf49162ebf13f41e
Dheerajp23 <59532632+Dheerajp23@users.noreply.github.com>
2025-02-10 21:58:31 +0530

    completed

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-const x = 29 + 48 + 59 + 18 + 28 + 44 + 19;
-const y = (x * 100) / 520;
+const bar = document.querySelector('.bar');
+    
+    
+    
+const show = document.querySelector('.show');
+document.querySelector('.bar').addEventListener('click', function() {
+    
+
+    if (show.style.visibility === 'hidden') {
+        bar.style.transform = 'rotate(90deg)';
+        bar.style.transition = 'linear 0.5s';
+        show.style.visibility = 'visible';
+        show.style.opacity = '1';
+        show.style.transition = 'linear 0.5s';
+    } else {
+        bar.style.transform = 'rotate(0deg)';
+        bar.style.transition = 'linear 0.5s';
+        show.style.visibility = 'hidden';
+        show.style.opacity = '0';
+        show.style.transition = 'linear 0.5s';
+    }
+}
+);
+
+
+document.querySelectorAll('.linkss').forEach(function(link) {
+    link.addEventListener('click', function() {
+        bar.style.transform = 'rotate(0deg)';
+        bar.style.transition = 'linear 0.5s';
+        show.style.visibility = 'hidden';
+        show.style.opacity = '0';
+        show.style.transition = 'linear 0.5s';
+    });
+});
